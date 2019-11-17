7798b2193d8d9a3f47d2e85018f47f11c766bf57
Luca Di Gaspero <l.digaspero@uniud.it>
2019-11-17 12:29:45 +0100

    Fixed sha256 for 2.8.5

--- ibexlib.rb
+++ ibexlib.rb
@@ -2,7 +2,7 @@ class Ibexlib < Formula
   desc "C++ library for constraint processing over real numbers"
   homepage "http://www.ibex-lib.org/"
   url "https://github.com/ibex-team/ibex-lib/archive/ibex-2.8.5.tar.gz"
-  sha256 "f88850be0a62836bd756ce5bac24d1817d939779c332fbdc41bf942472b2e025"
+  sha256 "d1ed009444aece20044744679e74057ac46f3eeeca59a8330def6f2f309558e9"
   head "https://github.com/ibex-team/ibex-lib.git"
 
   bottle do
